bcdc8aa561e14bda633037e476e56987ce6178f9
DenDrummer <muzikant96@gmail.com>
2017-02-13 16:34:16 +0100

    Add project files.

---
+++ Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User
+    {
+        public ushort Id { get; set; }
+        [Required]
+        [RegularExpression("^[a-z_]{4,12}$", ErrorMessageResourceName = "InvalidUserName", ErrorMessageResourceType = typeof(Resources))]
+        public string UserName { get; set; }
+    }
+}
---
+++ Domain/Viewer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Viewer
+    {
+        public ushort Id { get; set; }
+        public User User { get; set; }
+        public Streamer Streamer { get; set; }
+        public ushort ChatLevel { get; set; }
+    }
+}
