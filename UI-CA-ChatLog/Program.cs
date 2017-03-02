397c52f881e2fedcceb15d0534d844b314bc4635
DenDrummer <muzikant96@gmail.com>
2017-03-02 17:36:31 +0100

    0.0.17

--- UI-CA-ChatLog/Program.cs
+++ UI-CA-ChatLog/Program.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChatGame.BL;
+using ChatGame.BL.Domain;
+using System;
 
 namespace ChatGame.UI_CA_ChatLog
 {
     public class Program
     {
+        private static IGameManager mgr = new GameManager();
         static void Main(string[] args)
         {
-            Console.WriteLine("Whose Chat do you want to log?");
+            string loggedChat = GetChat();
+        }
+
+        private static string GetChat()
+        {
+            string chat = null;
+            while (!string.IsNullOrEmpty(chat))
+            {
+                Console.WriteLine(Resources.Resources.AskWhoToLog);
+                Console.Write("=> ");
+                string input = Console.ReadLine();
+                //the line below will give an error as it has not been implemented yet in the GameManager class
+                Streamer s = mgr.GetStreamer(input);
+                chat = s.User.UserName;
+            }
+            return chat;
         }
     }
 }
